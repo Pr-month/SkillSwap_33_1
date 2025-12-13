e093d22a96d17a7c4d7289ef94a77d867f297344
CLoudXVII <sharkov.vs@outlook.com>
2025-12-13 13:56:48 +0300

    fix(jwt.config): added refresh to .env, fixed the typization

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,29 +1,14 @@
 import { registerAs } from '@nestjs/config';
+import { JwtSignOptions } from '@nestjs/jwt';
 
 export const jwtConfig = registerAs('JWT_CONFIG', () => {
-  const expiresInStr = process.env.JWT_EXPIRES_IN || '3600';
-  const refreshExpiresInStr = process.env.JWT_REFRESH_EXPIRES_IN || (60 * 60 * 24 * 7).toString();
-
-  // Для обхода ошибки типизации useFactory в app.module
-  const expiresInNum = Number(expiresInStr);
-  const refreshExpiresInNum = Number(refreshExpiresInStr);
-
-  if (isNaN(expiresInNum) || expiresInNum <= 0) {
-    throw new Error(
-      `Недопустимое числовое значение для JWT_EXPIRES_IN: ${expiresInStr}. Должно быть положительное число в секундах.`,
-    );
-  }
-
-  if (isNaN(refreshExpiresInNum) || refreshExpiresInNum <= 0) {
-    throw new Error(
-      `Недопустимое числовое значение для JWT_REFRESH_EXPIRES_IN: ${refreshExpiresInStr}. Должно быть положительное число в секундах.`,
-    );
-  }
-
   return {
     secret: process.env.JWT_SECRET || 'jwtsecret',
-    expiresIn: expiresInNum,
+    expiresIn: (process.env.JWT_EXPIRES_IN ||
+      '1h') as JwtSignOptions['expiresIn'],
+
     refreshSecret: process.env.JWT_REFRESH_SECRET || 'refresh_secret',
-    refreshExpiresIn: refreshExpiresInNum,
+    refreshExpiresIn: (process.env.JWT_REFRESH_EXPIRES_IN ||
+      '7d') as JwtSignOptions['expiresIn'],
   };
 });
