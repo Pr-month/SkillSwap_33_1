a32cdf2c1e0664b80d70eb3ada41d5d536d9413a
Galina Bodunova <galinabodunova@MacBook-Air-Galina.local>
2025-12-14 14:50:28 +0700

    feat: update AppModule and AuthModule for global JWT config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,41 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { dbConfig } from './config/db.config';
+import { appConfig } from './config/app.config';
+import { jwtConfig } from './config/jwt.config';
+import { type JwtConfig, type DbConfig } from './config/types';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { SkillsModule } from './skills/skills.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    SkillsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, jwtConfig, dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [dbConfig.KEY],
+      useFactory: (cfg: DbConfig) => cfg,
+    }),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [jwtConfig.KEY],
+      useFactory: (cfg: JwtConfig) => ({
+        secret: cfg.secret,
+        signOptions: { expiresIn: cfg.expiresIn },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,13 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 import { UsersModule } from '../users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.register({
-      secret: process.env.JWT_ACCESS_SECRET || 'access-secret',
-      signOptions: { expiresIn: '15m' },
-    }),
-  ],
+  imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, AccessTokenStrategy, RefreshTokenStrategy],
 })
