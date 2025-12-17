eb052d3ad022f546ce8c98d4e7b5804183e0d66b
Nastysha <web.orlova.dev@gmail.com>
2025-12-17 13:58:43 +0300

    feat(config): валидация переменных окружения с Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from 'joi';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
@@ -23,6 +24,29 @@ import { FilesModule } from './files/files.module';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig, jwtConfig, dbConfig],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().default(3000),
+
+        DB_HOST: Joi.string().default('localhost'),
+        DB_PORT: Joi.number().default(5432),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+
+        JWT_SECRET: Joi.string().min(8).required(),
+        JWT_EXPIRES_IN: Joi.string().default('1h'),
+        JWT_REFRESH_SECRET: Joi.string().min(8).required(),
+        JWT_REFRESH_EXPIRES_IN: Joi.string().default('7d'),
+
+        HASH_SALT: Joi.number().default(10),
+      }),
+
+      validationOptions: {
+        abortEarly: false,
+      },
     }),
     TypeOrmModule.forRootAsync({
       inject: [dbConfig.KEY],
