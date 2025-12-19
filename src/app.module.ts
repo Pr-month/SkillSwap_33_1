fbed4145d6a160cf0fd55c1226b13ce8d83066a0
Nastysha <web.orlova.dev@gmail.com>
2025-12-19 10:40:35 +0300

    ref(config): вынесла валидацию env в отдельный файл

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as Joi from 'joi';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
@@ -10,6 +9,7 @@ import { AppController } from './app.controller';
 import { dbConfig } from './config/db.config';
 import { appConfig } from './config/app.config';
 import { jwtConfig } from './config/jwt.config';
+import { envValidationSchema } from './config/env.validation';
 import { type JwtConfig, type DbConfig } from './config/types';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -24,28 +24,10 @@ import { FilesModule } from './files/files.module';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig, jwtConfig, dbConfig],
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string()
-          .valid('development', 'production', 'test')
-          .default('development'),
-        PORT: Joi.number().default(3000),
-
-        DB_HOST: Joi.string().default('localhost'),
-        DB_PORT: Joi.number().default(5432),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
-        DB_NAME: Joi.string().required(),
-
-        JWT_SECRET: Joi.string().min(8).required(),
-        JWT_EXPIRES_IN: Joi.string().default('1h'),
-        JWT_REFRESH_SECRET: Joi.string().min(8).required(),
-        JWT_REFRESH_EXPIRES_IN: Joi.string().default('7d'),
-
-        HASH_SALT: Joi.number().default(10),
-      }),
-
+      validationSchema: envValidationSchema,
       validationOptions: {
         abortEarly: false,
+        allowUnknown: true,
       },
     }),
     TypeOrmModule.forRootAsync({
